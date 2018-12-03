c308694b185069e4b820c32a4d28ac922511b2fe
rap2h <raphaelht@gmail.com>
2018-12-03 01:27:45 +0100

    Updates

---
+++ precache-manifest.3d4b009f449edf6f1d91665de90b79c5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "08e735cbbdbdb8a8d379",
+    "url": "/pop-noel/static/js/runtime~main.08e735cb.js"
+  },
+  {
+    "revision": "d9bdf6416e266ec5a838",
+    "url": "/pop-noel/static/js/main.d9bdf641.chunk.js"
+  },
+  {
+    "revision": "2cab3507dae6401f0df4",
+    "url": "/pop-noel/static/js/1.2cab3507.chunk.js"
+  },
+  {
+    "revision": "d9bdf6416e266ec5a838",
+    "url": "/pop-noel/static/css/main.42e36640.chunk.css"
+  },
+  {
+    "revision": "824c8c0d32afe922087856ab055da968",
+    "url": "/pop-noel/index.html"
+  }
+];
