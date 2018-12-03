5fdcef8ae8205f1b35c5871839635bd4a587775f
rap2h <raphaelht@gmail.com>
2018-12-03 01:26:55 +0100

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,49 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import Doc from "./Doc";
+
+const docs = [
+  "joconde/07430003391",
+  "joconde/07350000494",
+  "joconde/5002E012518",
+  "memoire/AP0602T05982",
+  "memoire/IVR54_99172513NUCA",
+  "memoire/AP0602T04393",
+  "memoire/AP71L20602",
+  "joconde/00000100554",
+  "joconde/5002E009430",
+  "joconde/M0418002994",
+  "joconde/M0352003093",
+  "joconde/M0333012292",
+  "memoire/IVR94_20092A01074",
+  "memoire/IVR26_20037101092",
+  "joconde/00980000413",
+"memoire/APNADAR011656",
+"joconde/00000099832",
+"joconde/50430001541",
+"memoire/IVR11_20099500040",
+"joconde/07760000924",
+"memoire/AP62T000063",
+"memoire/AP62T000066",
+"memoire/APFSK50-09",
+"joconde/M0432000514"
+];
 
 class App extends Component {
+  renderDocs() {
+    return docs.map(d => <Doc key={d} uri={d} />);
+  }
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.tsx</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
+        <h1>🎄 Joyeux Noël ! 🎄</h1>
+        <section className="docs">{this.renderDocs()}</section>
+        <div>
+          Données :{" "}
+          <a href="http://pop.culture.gouv.fr">
+            Plateforme Ouverte du Patrimoine
           </a>
-        </header>
+        </div>
       </div>
     );
   }
---
+++ src/Doc.tsx
@@ -0,0 +1,55 @@
+import React, { Component } from "react";
+
+const apiUrl = "http://pop-api.eu-west-3.elasticbeanstalk.com/";
+const popUrl = "http://pop.culture.gouv.fr/notice/";
+const imgUrl = "https://s3.eu-west-3.amazonaws.com/pop-phototeque/";
+
+interface Props {
+  uri: string;
+}
+
+interface State {
+  data?: DocData;
+}
+
+interface DocData {
+  IMG: [string] | string;
+  TICO: string;
+  TITR: string;
+  LEG: string;
+}
+
+class Doc extends Component<Props> {
+  state: State = {};
+  async componentDidMount() {
+    const response = await fetch(`${apiUrl}${this.props.uri}`);
+    const json = await response.json();
+    this.setState({ data: json });
+  }
+  img(data: DocData): string {
+    if (Array.isArray(data.IMG)) {
+      return imgUrl + data.IMG[0];
+    }
+    return data.IMG;
+  }
+  title(data: DocData): string {
+    return (data.LEG || data.TICO || data.TITR)
+      .replace(/\/.*/, "")
+      .replace(" : ", "\n");
+  }
+  render() {
+    if (!this.state.data) {
+      return <div />;
+    }
+    return (
+      <div className="doc">
+        <a href={popUrl + this.props.uri} target="_blank">
+          <img className="frame" src={this.img(this.state.data)} />
+          <div className="card">{this.title(this.state.data)}</div>
+        </a>
+      </div>
+    );
+  }
+}
+
+export default Doc;
